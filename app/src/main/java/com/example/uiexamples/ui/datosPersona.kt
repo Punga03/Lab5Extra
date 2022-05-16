f7eadbba1c5abebaa3c48fcd973d89d784859883
Yoschua V <yoschuavil@gmail.com>
2022-05-16 10:39:17 -0600

    Agrego la ventana del Login y el update de los datos

---
+++ app/src/main/java/com/example/uiexamples/ui/datosPersona.kt
@@ -0,0 +1,12 @@
+package com.example.uiexamples.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.uiexamples.R
+
+class datosPersona : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos_persona)
+    }
+}
